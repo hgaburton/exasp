c86ea387d4ce2da33a19fbf0df0ac70c9c7f56ec
Hugh Burton <hgaburton@gmail.com>
2025-11-19 12:40:29 +0000

    Updated hubbard_tups and added example for exact_time_evolution

--- exasp/exact_time_evolution.py
+++ exasp/exact_time_evolution.py
@@ -2,115 +2,104 @@
 import sys
 import numpy as np
 from scipy.sparse.linalg import eigsh, expm_multiply
-from system import MolecularSystem
-from path import SinNPath
+from utils.system import MolecularSystem
+from utils.path import SinNPath
 from datetime import datetime
 np.set_printoptions(linewidth=1000,suppress=True,precision=6)
-
 from argparse import ArgumentParser
-parser = ArgumentParser()
-parser.add_argument("-mol", "--xyzfile", dest="xyzfile",
-                    help="Molecule .xyz file")
-parser.add_argument("-b", "--basis", dest="basis", default='sto-3g', type=str,
-                    help="Basis set")
-parser.add_argument("-lmax", "--lambda_max", dest="lmax", default=1.0, type=float,
-                    help = "Coupling strengths")
-parser.add_argument("-wmax", "--omega_max", dest="wmax", default=1.0, type=float,
-                    help = "Maximum value of omega")
-parser.add_argument("-pol", "--polarisation", dest="eps", default='0,0,1', type=str,
-                    help = "Polarisation vector in (x,y,z) format")
-parser.add_argument("-n", "--nstep", dest="nstep", default=101, type=int,
-                    help = "Number timesteps")
-parser.add_argument("-T", dest="T", default=101, type=float,
-                    help = "Total time evolution")
-parser.add_argument('--nphoton', dest='nphoton', default=1, type=int,
-                    help = "Maximum number of allowed photons")
-parser.add_argument('--init', dest='initial_state', default='0,0', type=str,
-                    help = "Initial state 'n,m' where n is the electronic state and m is the photon number")
-parser.add_argument("-o", "--order", dest="order", default=3, type=float,
-                    help = "Power order of the sin function for lambda path")
-parser.add_argument("-f", "--nfrozen", dest="nfrozen", default=0, type=int,
-                    help = "Number of frozen core orbitals.")
-args = parser.parse_args()
 
-print(f'##########################################################')
-print(f'# Performing exact time evolution for excited-state ASP  ')
-print(datetime.now().strftime("# Today: %d %b %Y at %H:%M:%S"))
-print('#', args)
-print(f'##########################################################')
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Perform exact time evolution along an excited-state adiabatic state preparation path for a molecular system.")
+    parser.add_argument("-mol",dest="xyzfile",help="Molecule .xyz file")
+    parser.add_argument("-b",dest="basis",default='sto-3g',type=str,help="Basis set [default=sto-3g]")
+    parser.add_argument("-lmax",dest="lmax",default=1.0,type=float,help="Coupling strengths maximum value [default=1.0]")
+    parser.add_argument("-wmax",dest="wmax",default=1.0,type=float,help="Maximum value of omega [default=1.0]")
+    parser.add_argument("-pol","--polarisation",dest="eps",default='0,0,1',type=str,help="Polarisation vector in (x,y,z) format [default=0,0,1]")
+    parser.add_argument("-n",dest="nstep",default=101,type=int,help="Number timesteps [default=101]")
+    parser.add_argument("-T",dest="T",default=101,type=float,help="Total time evolution [default=101]")
+    parser.add_argument('--nphoton',dest='nphoton',default=1,type=int,help="Maximum number of allowed photons [default=1]")
+    parser.add_argument('--init',dest='initial_state',default='0,0',type=str,help="Initial state 'n,m' where n is the electronic state and m is the photon number [default=0,0]")
+    parser.add_argument("-o",dest="order",default=3,type=float,help="Power order of the sin function for lambda path [default=3]")
+    parser.add_argument("-f",dest="nfrozen",default=0,type=int,help="Number of frozen core orbitals [default=0]")
+    args = parser.parse_args()
 
-# Get polarisation vector
-# NOTE: The ordering of the cartesian components is Z/X/Y, which matches the 
-#       ordering of spherical harmonics in the ORCA format.
-assert(len(args.eps.split(',')) == 3)
-eps = list(map(float,args.eps.split(',')))
-assert(len(args.initial_state.split(',')) == 2)
-(init_e,init_p) = map(int,args.initial_state.split(','))
+    print(f'##########################################################')
+    print(f'# Performing exact time evolution for excited-state ASP  ')
+    print(datetime.now().strftime("# Today: %d %b %Y at %H:%M:%S"))
+    print('#', args)
+    print(f'##########################################################')
 
-# Setup the EXASP system
-system = MolecularSystem(args.xyzfile,args.basis,args.nphoton,eps,nfrozen=args.nfrozen)
+    # Get polarisation vector
+    assert(len(args.eps.split(',')) == 3)
+    eps = list(map(float,args.eps.split(',')))
+    assert(len(args.initial_state.split(',')) == 2)
+    (init_e,init_p) = map(int,args.initial_state.split(','))
 
-# Set up the path definition
-path = SinNPath(args.wmax,args.lmax,args.order)
+    # Setup the EXASP system
+    system = MolecularSystem(args.xyzfile,args.basis,args.nphoton,eps,nfrozen=args.nfrozen)
 
-# Print the molecules
-print(' Molecule [angstrom]:')
-print(' --------------------------------------------------------')
-with open(args.xyzfile) as f:
-    for l in f.readlines():
-        print("  ",l,end='')
+    # Set up the path definition
+    path = SinNPath(args.wmax,args.lmax,args.order)
 
-# Initial state
-einit, vinit = system.solve_molecular_hamiltonian()
-vk = np.kron(np.eye(args.nphoton+1,1,k=-init_p,dtype=complex).flatten(), vinit[:,init_e])
+    # Print the molecules
+    print(' Molecule [angstrom]:')
+    print(' --------------------------------------------------------')
+    with open(args.xyzfile) as f:
+        for l in f.readlines():
+            print("  ",l,end='')
 
-# Time evolution
-T = args.T
-n = args.nstep
-dt = T / n
+    # Initial state
+    einit, vinit = system.solve_molecular_hamiltonian()
+    vk = np.kron(np.eye(args.nphoton+1,1,k=-init_p,dtype=complex).flatten(), vinit[:,init_e])
 
-# Print details of the calculation
-print()
-print(f' Parameters:')
-print(' --------------------------------------------------------')
-print(f"  Number of photons    = {args.nphoton: 10d}")
-print(f"  Total time evolution = {args.T: 10.6e}")
-print(f"  Number of steps      = {args.nstep: 10d}")
-print(f"  Time step            = {dt: 10.6e}")
-print(f'  Initial state        = |e{init_e},p{init_p}>')
-print(f'  Polarisation vector  = {eps}')
-print(f'  lmax                 = {args.lmax}')
-print(f'  wmax                 = {args.wmax}')
-print(f'  order                = {args.order}')
+    # Time evolution
+    T = args.T
+    n = args.nstep
+    dt = T / n
 
-print()
-print(' --------------------------------------------------------')
-print(f'  {"t_k":^14s}   {"<E(t_k)>":^18s} {"<Pn(t_k)>":^12s}   State fidelity -->')
-print(' --------------------------------------------------------')
-sys.stdout.flush()
+    # Print details of the calculation
+    print()
+    print(f' Parameters:')
+    print(' --------------------------------------------------------')
+    print(f"  Number of photons    = {args.nphoton: 10d}")
+    print(f"  Total time evolution = {args.T: 10.6e}")
+    print(f"  Number of steps      = {args.nstep: 10d}")
+    print(f"  Time step            = {dt: 10.6e}")
+    print(f'  Initial state        = |e{init_e},p{init_p}>')
+    print(f'  Polarisation vector  = {eps}')
+    print(f'  lmax                 = {args.lmax}')
+    print(f'  wmax                 = {args.wmax}')
+    print(f'  order                = {args.order}')
+    print(f'  Basis set            = {args.basis}')
+    print(f'  Frozen core orbitals = {args.nfrozen}')
 
-v0 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,0])
-v1 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,1])
-v2 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,2])
-v3 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,3])
-
-for i,k in enumerate(np.linspace(0,1,n+1)):
-    # Get values of w and l
-    wk, lk = path.w(k), path.l(k)
-    # Hamiltonian
-    Hk = system.H(wk,lk)
-    H0 = system.H(wk,0)
-    # Time propagation
-    vk = expm_multiply(-1j * dt * (Hk-H0), vk) 
-    # Instantaneous energy
-    Ek = np.dot(np.conj(vk), Hk @ vk).real
-    # Instantaneous photon number
-    Pk = np.dot(np.conj(vk), system.Hp_c @ vk).real
-    # Fidelity with different states
-    f0 = np.abs(np.dot(v0, vk))**2 
-    f1 = np.abs(np.dot(v1, vk))**2 
-    f2 = np.abs(np.dot(v2, vk))**2 
-    f3 = np.abs(np.dot(v3, vk))**2 
-    print(f'  {k: 8.6e}   {Ek: 16.10f}   {Pk: 10.6e}   {f0: 10.6e}   {f1: 10.6e}   {f2: 10.6e}   {f3: 10.6e}')
+    print()
+    print(' --------------------------------------------------------')
+    print(f'  {"t_k":^14s}   {"<E(t_k)>":^18s} {"<Pn(t_k)>":^12s}   State fidelity -->')
+    print(' --------------------------------------------------------')
     sys.stdout.flush()
-print(' --------------------------------------------------------')
+
+    v0 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,0])
+    v1 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,1])
+    v2 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,2])
+    v3 = np.kron(np.eye(args.nphoton+1,1).flatten(), vinit[:,3])
+
+    for i,k in enumerate(np.linspace(0,1,n+1)):
+        # Get values of w and l
+        wk, lk = path.w(k), path.l(k)
+        # Hamiltonian
+        Hk = system.H(wk,lk)
+        # Time propagation
+        vk = expm_multiply(-1j * dt * Hk, vk) 
+        # Instantaneous energy
+        Ek = np.dot(np.conj(vk), Hk @ vk).real
+        # Instantaneous photon number
+        Pk = np.dot(np.conj(vk), system.Hp_c @ vk).real
+        # Fidelity with different states
+        f0 = np.abs(np.dot(v0, vk))**2 
+        f1 = np.abs(np.dot(v1, vk))**2 
+        f2 = np.abs(np.dot(v2, vk))**2 
+        f3 = np.abs(np.dot(v3, vk))**2 
+        print(f'  {k: 8.6e}   {Ek: 16.10f}   {Pk: 10.6e}   {f0: 10.6e}   {f1: 10.6e}   {f2: 10.6e}   {f3: 10.6e}')
+        sys.stdout.flush()
+    print(' --------------------------------------------------------')
--- exasp/hubbard_tups.py
+++ exasp/hubbard_tups.py
@@ -1,63 +1,52 @@
 #!/usr/bin/env python
-import sys
 from argparse import ArgumentParser
 from datetime import datetime
-import math
 import numpy as np
-from qiskit.quantum_info import Statevector
-from scipy.sparse.linalg import expm_multiply, eigsh
-from system import HubbardSystem
-from path import SinNPath
-import quantum
-
+import utils.quantum as quantum
+from utils.system import HubbardSystem
 np.set_printoptions(linewidth=1000,suppress=True,precision=6)
 
-parser = ArgumentParser()
-parser.add_argument("-U", dest="U", type=float,
-                    help="Hubbard U parameter")
-parser.add_argument("-nelec", dest="nelec", type=str, 
-                    help = "Number of electrons in (nalfa,nbeta) format")
-parser.add_argument("-dim", dest="dim", default='1,1,1', type=str,
-                    help = "Hubbard lattice dimensions")
-parser.add_argument("--site", dest="site", default=False, action='store_true',
-                    help = "This flag sets the calculation to work in the site basis")
-parser.add_argument("-f", "--nfrozen", dest="nfrozen", default=0, type=int,
-                    help = "Number of frozen core orbitals.")
-parser.add_argument("-pp", "--perfect_pairing", dest="pp", default=False, action='store_true',
-                    help = "Use a perfect pairing reference state")
-args = parser.parse_args()
+if __name__=="__main__":
+    parser = ArgumentParser(description="Generate tUPS input files for Hubbard lattice")
+    parser.add_argument("-U", dest="U", type=float, default=1.0, help="Hubbard U parameter [default=1.0]")
+    parser.add_argument("-nelec", dest="nelec", type=str, default="1,1", help="Number of electrons in (nalfa,nbeta) format [default=1,1]")
+    parser.add_argument("-dim", dest="dim", type=str, default="1,1,1", help="Hubbard lattice dimensions [default=1,1,1]")
+    parser.add_argument("--site", dest="site", action="store_true", default=False, help="This flag sets the calculation to work in the site basis [default=False]")
+    parser.add_argument("-f", dest="nfrozen", type=int, default=0, help="Number of frozen core orbitals [default=0]")
+    parser.add_argument("-pp", dest="pp", action="store_true", default=False, help="Use a perfect pairing reference state [default=False]")
+    args = parser.parse_args()
 
-print('##########################################################')
-print('# Generating tUPS input files for Hubbard lattice         ')
-print(datetime.now().strftime("# Today: %d %b %Y at %H:%M:%S"))
-print('#', args)
-print('##########################################################')
+    print('##########################################################')
+    print('# Generating tUPS input files for Hubbard lattice         ')
+    print(datetime.now().strftime("# Today: %d %b %Y at %H:%M:%S"))
+    print('#', args)
+    print('##########################################################')
 
-# Get polarisation vector
-eps = [0,0,0]
+    # Get polarisation vector
+    eps = [0,0,0]
 
-# Read in and process information about the Hubbard lattice
-assert(len(args.dim.split(','))==3)
-dim = list(map(int,args.dim.split(',')))
-assert(len(args.nelec.split(','))==2)
-ne  = list(map(int,args.nelec.split(',')))
+    # Read in and process information about the Hubbard lattice
+    assert(len(args.dim.split(','))==3)
+    dim = list(map(int,args.dim.split(',')))
+    assert(len(args.nelec.split(','))==2)
+    ne  = list(map(int,args.nelec.split(',')))
 
-# Setup the HubbardSystem
-nsite = dim[0]*dim[1]*dim[2]
-if(args.site):
-    mo_coeff = np.identity(nsite)
-    system = HubbardSystem(args.U,1,1,[0,0,0],ne,dim,mo_coeff=mo_coeff)
-else:
-    system = HubbardSystem(args.U,1,1,[0,0,0],ne,dim)
+    # Setup the HubbardSystem
+    nsite = dim[0]*dim[1]*dim[2]
+    if(args.site):
+        mo_coeff = np.identity(nsite)
+        system = HubbardSystem(args.U,1,1,[0,0,0],ne,dim,mo_coeff=mo_coeff)
+    else:
+        system = HubbardSystem(args.U,1,1,[0,0,0],ne,dim)
 
 
-# Initial state
-einit0, vinit = system.solve_molecular_hamiltonian()
-np.savetxt("ham.eigval", einit0, fmt="% 20.16f")
-hmat, dip_hmat = quantum.get_2nd_quant_hamiltonian(system.mo_ints, system.ints.nmo()-system.nfrozen, eps)
+    # Initial state
+    einit0, vinit = system.solve_molecular_hamiltonian()
+    np.savetxt("ham.eigval", einit0, fmt="% 20.16f")
+    hmat, dip_hmat = quantum.get_2nd_quant_hamiltonian(system.mo_ints, system.ints.nmo()-system.nfrozen, eps)
 
-# Save for tUPS
-tUPS = quantum.tUPSAdaptor(nsite)
-tUPS.write_hamiltonian(hmat)
-tUPS.write_operators()
-tUPS.write_reference(ne[0],ne[1],perfect_pairing=args.pp)
+    # Save for tUPS
+    tUPS = quantum.tUPSAdaptor(nsite)
+    tUPS.write_hamiltonian(hmat)
+    tUPS.write_operators()
+    tUPS.write_reference(ne[0],ne[1],perfect_pairing=args.pp)
